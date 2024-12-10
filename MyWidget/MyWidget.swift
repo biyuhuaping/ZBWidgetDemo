b701738764cb21b8c7be2b1525fb0aea04c60c9c
zb <biyuhuaping@163.com>
2024-12-10 20:15:19 +0800

    姓名、年龄、性别

--- MyWidget/MyWidget.swift
+++ MyWidget/MyWidget.swift
@@ -9,17 +9,20 @@ import WidgetKit
 import SwiftUI
 
 private struct Provider: IntentTimelineProvider {
+    /// 提供占位视图，用于在小组件加载内容之前显示的默认内容
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), bgColor: .orange)
+        SimpleEntry(date: Date(), configuration: MyConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: BackgroundColorSelectionIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date(), bgColor: .orange)
+    /// 提供小组件的快照视图，用于小组件的预览或快速加载场景
+    func getSnapshot(for configuration: MyConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
-    func getTimeline(for configuration: BackgroundColorSelectionIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        let entries = [SimpleEntry(date: Date(), bgColor: configuration.bgColor)]
+    /// 提供小组件的时间线数据，用于定时刷新小组件的内容
+    func getTimeline(for configuration: MyConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        let entries = [SimpleEntry(date: Date(), configuration: configuration)]
         let timeline = Timeline(entries: entries, policy: .never)
         completion(timeline)
     }
@@ -27,24 +30,44 @@ private struct Provider: IntentTimelineProvider {
 
 private struct SimpleEntry: TimelineEntry {
     let date: Date
-    let bgColor: BackgroundColor
+    let configuration: MyConfigurationIntent
 }
 
+// 使用自定义视图显示小组件内容
 private struct IntentWidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
+        let configuration = entry.configuration
         ZStack {
             bgColor
-            Text(entry.date, style: .date)
-                .font(.subheadline)
-                .multilineTextAlignment(.center)
+            VStack(
+                alignment: .leading,
+                content: {
+                    Text(entry.date, style: .date)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                    Text("姓名：\(configuration.name ?? "无")")
+                    Text("年龄：\(configuration.age ?? 0)")
+                    Text("性别：\(getGender())")
+                })
         }
     }
-
+    
+    func getGender() -> String {
+        switch entry.configuration.gender {
+        case .man:
+            return "男"
+        case .woman:
+            return "女"
+        case .unknown:
+            return "未知"
+        }
+    }
+    
     @ViewBuilder
     private var bgColor: some View {
-        switch entry.bgColor {
+        switch entry.configuration.bgColor {
         case .blue:
             Color.blue
         case .red:
@@ -62,7 +85,8 @@ private struct IntentWidgetEntryView: View {
 struct MyWidget: Widget {
     private let kind: String = "MyWidget"
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: BackgroundColorSelectionIntent.self, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: MyConfigurationIntent.self, provider: Provider()) { entry in
+            // 使用自定义视图显示小组件内容
             IntentWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("MyWidget")
